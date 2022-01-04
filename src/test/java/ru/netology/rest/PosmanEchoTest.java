5c904abece239a56949da04b9d5e2da9f0fdfc95
Anna Sotnikova <ruceba77@gmail.com>
2022-01-04 22:58:49 +0600

    Commit

--- src/test/java/ru/netology/rest/PosmanEchoTest.java
+++ src/test/java/ru/netology/rest/PosmanEchoTest.java
@@ -19,7 +19,7 @@ public class PosmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("test1"))
+                .body("data.value", equalTo("test1"))
         ;
     }
 }
